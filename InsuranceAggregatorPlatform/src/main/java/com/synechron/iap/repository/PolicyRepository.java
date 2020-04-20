58c5b1c1cf0aa9690ef4e7b607fbfd241aafdc33
AshmiPawar1 <Ashmi.Pawar@synechron.com>
2020-04-20 16:15:31 +0530

    first commit

--- InsuranceAggregatorPlatform/src/main/java/com/synechron/iap/repository/PolicyRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.synechron.iap.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.synechron.iap.model.Policy;
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-
-}
